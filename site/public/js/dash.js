1bcc436269ee931b5308cd036f4f0241a21d4090
felipyroma <felipy.roma@sptech.school>
2023-04-08 02:58:05 -0300

    side menu updated

--- site/public/js/dash.js
+++ site/public/js/dash.js
@@ -1,21 +1,10 @@
-// function openMenu(){
-//     let leftMenu = document.getElementsByClassName("left_menu") 
-//     let bars = document.getElementById("bars")
-
-//     leftMenu.style.display = "flex";
-//     leftMenu.style.transition = "10s";
-//     bars.style.display = "none";
-
-// }
 
 function showMenu() {
-    let element = document.getElementById("left_menu");
-    element.classList.add("show");
-    bars.style.display = "#274360";
+    menu.style.display = "none";
+    left_menu.style.display = "block";
 }
 
 function hideMenu() {
-    let element = document.getElementById("left_menu");
-    element.classList.remove("show");
-    bars.style.display = "block";
+    menu.style.display = "block";
+    left_menu.style.display = "none";
 }
