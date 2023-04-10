51017b5b51e9208f71fe3ab9136bfe5a88570158
felipyroma <felipy.roma@sptech.school>
2023-04-10 00:41:38 -0300

    grafic started

--- site/public/js/dash.js
+++ site/public/js/dash.js
@@ -1,4 +1,4 @@
-
+// mostrar o menu lateral
 function showMenu() {
     left_menu.style.display = "block";
 }
@@ -6,3 +6,21 @@ function showMenu() {
 function hideMenu() {
     left_menu.style.display = "none";
 }
+
+// plotar dados dos graficos
+ // test
+ let p1 = 43;
+ let p2 = 67;
+ let p3 = 90;
+
+ let rest1 = 100;
+ let rest2 = 100;
+ let rest3 = 100;
+
+ rest1 -= p1;
+ rest2 -= p2;
+ rest3 -= p3;
+
+ percentage1.innerHTML = p1 + "%";
+ percentage2.innerHTML = p2 + "%";
+ percentage3.innerHTML = p3 + "%";
