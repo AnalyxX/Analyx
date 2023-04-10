982ad845bc6a0906c8ccf1d6d4389a23f147394d
felipyroma <felipy.roma@sptech.school>
2023-04-09 21:13:46 -0300

    sidebar agora vai

--- site/public/js/dash.js
+++ site/public/js/dash.js
@@ -1,10 +1,8 @@
 
 function showMenu() {
-    menu.style.display = "none";
     left_menu.style.display = "block";
 }
 
 function hideMenu() {
-    menu.style.display = "block";
     left_menu.style.display = "none";
 }
