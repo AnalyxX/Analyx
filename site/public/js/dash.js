027a99cb51afbd15dd29210e7246c5cb6f800835
felipyroma <felipy.roma@sptech.school>
2023-04-12 01:09:42 -0300

    aligning grafics

--- site/public/js/dash.js
+++ site/public/js/dash.js
@@ -8,19 +8,7 @@ function hideMenu() {
 }
 
 // plotar dados dos graficos
- // test
- let p1 = 43;
- let p2 = 67;
- let p3 = 90;
 
- let rest1 = 100;
- let rest2 = 100;
- let rest3 = 100;
+let total = 57
+let usedDisc = 43
 
- rest1 -= p1;
- rest2 -= p2;
- rest3 -= p3;
-
- percentage1.innerHTML = p1 + "%";
- percentage2.innerHTML = p2 + "%";
- percentage3.innerHTML = p3 + "%";
